6c4868126cf543b0c0133999a26f4cae541c03b7
Jeremy Allan <jallan8210@gmail.com>
2023-08-23 18:21:32 -0400

    added some nicer example elements

--- components/ExampleElements.jsx
+++ components/ExampleElements.jsx
@@ -55,11 +55,50 @@ function ExampleElements({ numElements = 100 }) {
     <>
       {elements}
       
-      <div className='example-message-element'>warning</div>
-      <div className='example-message-element recieved'>recieved</div>
-      <div className='example-message-element sent'>sent</div>
+      <div className="chat chat-start">
+        <div className="chat-bubble chat-bubble-primary">What kind of nonsense is this</div>
+      </div>
+      <div className="chat chat-start">
+        <div className="chat-bubble chat-bubble-secondary">Put me on the Council and not make me a Master!??</div>
+      </div>
+      <div className="chat chat-start">
+        <div className="chat-bubble chat-bubble-accent">That's never been done in the history of the Jedi. It's insulting!</div>
+      </div>
+      <div className="chat chat-end">
+        <div className="chat-bubble chat-bubble-info">Calm down, Anakin.</div>
+      </div>
+      <div className="chat chat-end">
+        <div className="chat-bubble chat-bubble-success">You have been given a great honor.</div>
+      </div>
+      <div className="chat chat-end">
+        <div className="chat-bubble chat-bubble-warning">To be on the Council at your age.</div>
+      </div>
+      <div className="chat chat-end">
+        <div className="chat-bubble chat-bubble-error">It's never happened before.</div>
+      </div>
+
+      {/* <ExampleElement /> */}
     </>
   )
 }
 
+function ExampleElement(props) {
+  const { numElements, senderClass, colourClass } = props;
+  return (
+    <div key={numElements} className={`example-message-element chat ${senderClass || `chat-start`}`}>
+      <FontAwesomeIcon icon="fa-solid fa-user" />
+      <div className={`chat-bubble ${colourClass || 'chat-bubble-error'}`}>
+          wee Special Element! also, {numElements} 
+      </div>
+      {/* <div className='example-message-text'></div> */}
+    </div>
+  )
+}
+
 export default ExampleElements;
+
+{/* <div className={`example-message-element chat ${senderClass()}`}>
+        <div className={`chat-bubble ${colourClass()}`}>
+          hello world hello world 
+        </div>
+      </div> */}
