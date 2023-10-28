7d6025d3c18d9986fb51868b8a0a69eb270e0f0b
Jeremy Allan <jallan8210@gmail.com>
2023-10-27 20:00:55 -0400

    first commit of new ui branch

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -14,6 +14,13 @@ import toast from "react-hot-toast";
 import { validateCommand } from "@/util/functions";
 import { roll } from "@/app/test-page/rolls";
 
+const coolImages = require("cool-images");
+
+
+// const getRandomCat = require('random-cat-img'); // this is broken for some reason
+// const randomPuppy = require('random-puppy');
+
+
 export default function Page() {
   const [ user ] = useAuthState(userAuth);
   let gameId = user ? accountLocalStorage.getCurrentGame(user?.uid) : false;
@@ -59,16 +66,56 @@ export default function Page() {
   }
 
   return (
-    <PageLayout title="Welcome to PartyUp!">
-      {(user) && (
-        <>
-          <p className="btn" id={ui.home.userName.id} onClick={test}> Welcome {user.displayName}!</p>
-          <Dir.game.current className="btn mt-5" {...props}/>
-          <Dir.game.create className="btn mt-5"/>
-          <Dir.user.active className="btn mt-5"/>
-          {/*<Dir.game.public className="btn mt-5"/>*/}
-        </>)
-      }
-    </PageLayout>
+    <div className='home-page w-full h-full border'>
+      <div className='border'>
+        <h1 className='text-8xl font-medium'>welcome to tavern!</h1>
+
+        <div>weve got this,</div>
+
+        <div>and this</div>
+
+        <div>aand this</div>
+
+        <div>aaand this</div>
+      </div>
+
+      <div className='home-discover-region flex flex-col gap-8 py-12 border'>
+        <div className='text-5xl font-medium'>check out our games!</div>
+
+        <div className='game-card-gallery grid grid-cols-4 m-auto gap-8 w-4/5'>
+          <GameCard />
+          <GameCard />
+          <GameCard />
+          <GameCard />
+        </div>
+      </div>
+
+      <div className='border'>
+      </div>
+    </div>
+  )
+}
+
+
+
+function GameCard() {
+
+  const randomImg = coolImages.one(800, 800);
+  
+  return (
+    <div className='game-card flex flex-col col-span-1 justify-center bg-neutral rounded-md overflow-clip'>
+      <div className='flex flex-col text-center bg-primary h-12 text-lg'>
+        <div className='m-auto'>game name</div>
+      </div>
+
+      <div className='game-card__content flex flex-col p-4 gap-2'>
+        <div>hello world this is a game</div>
+
+        <img className='text-center aspect-square w-full bg-red-400 rounded shadow-lg shadow-base-200'
+          src={randomImg} />
+        
+        <p className='text-sm'>height fact recall student method community private hat electricity touch single shelf name exactly depend select art official win border buried pot differ glad</p>
+      </div>
+    </div>
   )
 }
--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -8,34 +8,111 @@ import { googleSignInPopUp, userAuth } from "@/firebase/base";
 
 import { ui } from "@/util/ui";
 
-export default function NavBar(props) {
+export default function Navbar(props) {
   return (
-    <div className={`${props.className} header-nav navbar bg-base-100`}>
-      <div className="navbar-start">
-        <Dir.home>
-          <button className="btn btn-ghost normal-case text-xl">PartyUp!</button>
-        </Dir.home>
-      </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          {/*<li></li>*/}
-          {/*<li tabIndex={0}>*/}
-          {/*    <details>*/}
-          {/*        <summary>Parent</summary>*/}
-          {/*        <ul className="p-2">*/}
-          {/*            <li><a>Submenu 1</a></li>*/}
-          {/*            <li><a>Submenu 2</a></li>*/}
-          {/*        </ul>*/}
-          {/*    </details>*/}
-          {/*</li>*/}
-          {/*<li><Dir.game/></li>*/}
+    <div className="navbar bg-neutral z-10">
+
+      <NavbarStart />
+      <NavbarCenter />
+      <NavbarEnd />
+      
+    </div>
+  )
+}
+
+
+function NavbarStart(props) {
+  // const { item } = props;
+
+  return (
+    <div className="navbar-start">
+
+      <div className="dropdown">
+        <label tabIndex={0} className="btn btn-ghost lg:hidden">
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+        </label>
+
+        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+          <li><a>Discover</a></li>
+          <li><a>Games</a></li>
+          <li>
+            <a>Create</a>
+            <ul className="p-2">
+              <li><a>Character</a></li>
+              <li><a>World</a></li>
+            </ul>
+          </li>
+          <li><a>Settings</a></li>
         </ul>
       </div>
-      <div className="navbar-end">
-        <Login/>
-        <Logout/>
+
+      <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+    </div>
+  )
+}
+
+function NavbarCenter(props) {
+  // const { item } = props;
+
+  return (
+    <div className="navbar-center hidden lg:flex">
+      <ul className="menu menu-horizontal px-1">
+        <li><a href='/discover'>Discover</a></li>
+        <li><a href='/play'>Play</a></li>
+        <li tabIndex={0}>
+          <details>
+            <summary>Create</summary>
+            <ul className="p-2 bg-accent text-accent-content">
+              <li>create a...</li>
+              <li><a className='navbar__link--error'>Character</a></li>
+              <li><a className='navbar__link--error'>World</a></li>
+              <li><a className='navbar__link--error'>Game</a></li>
+              <li><a className='navbar__link--error'>Community</a></li>
+            </ul>
+          </details>
+        </li>
+        <li><a>Games</a></li>
+        {/* <li><a>Community</a></li> */}
+        <li tabIndex={1}>
+          <details>
+            <summary>Community</summary>
+            <ul className="p-2 bg-accent text-accent-content">
+              <li className='px-4'>coming soon!</li>
+            </ul>
+          </details>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export function NavbarEnd(props) {
+  // const { item } = props;
+
+  return (
+    <div className="navbar-end">
+      {/* <a className="btn">Button</a> */}
+      <div className="dropdown dropdown-end">
+        <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+          <div className="w-10 rounded-full">
+            <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+          </div>
+        </label>
+
+        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+          <li>
+            <a className="justify-between">
+              Profile
+              <span className="badge">New</span>
+            </a>
+          </li>
+          <li><a href='/user-settings'>Settings</a></li>
+          <li><a>Logout</a></li>
+        </ul>
+
       </div>
     </div>
+
   )
 }
 
